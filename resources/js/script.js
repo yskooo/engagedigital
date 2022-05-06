399e3c4cf4a28813464d715aeac80d3131467a6a
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2022-05-06 22:21:34 +0800

    Upload resources files

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+// function for burger menu
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active')
+    })
+    // animation for sticky navigation bar
+const sticky = document.querySelector('.navbar');
+
+if (sticky.style.position !== 'sticky') {
+    let stickyTop = sticky.offsetTop;
+
+    document.addEventListener('scroll', () => {
+        window.scrollY >= stickyTop ?
+            sticky.classList.add('fixed') :
+            sticky.classList.remove('fixed');
+    });
+}
